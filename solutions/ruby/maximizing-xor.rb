20682ef89f653f115229f30c1f9b98f1dd806027
Peter Prakobkit <kprakobkit@gmail.com>
2014-11-22 23:14:37 -0800

    refactor maximizing-xor

--- solutions/ruby/maximizing-xor.rb
+++ solutions/ruby/maximizing-xor.rb
@@ -1,13 +1,13 @@
 def maxXor(l,r)
-  max = 0
-  (l..r).each do |left_bound|
-    (l..r).each do |right_bound|
-      xor_value = left_bound ^ right_bound
-      max = xor_value > max ? xor_value : max
-    end
-  end
+  # (l..r).each do |left_bound|
+  #   (l..r).each do |right_bound|
+  #     xor_value = left_bound ^ right_bound
+  #     max = xor_value > max ? xor_value : max
+  #   end
+  # end
 
-  max
+  (l..r).to_a.combination(2).map {|x,y| x ^ y}.max
+
 end
 
 p maxXor(1,10) == 15
