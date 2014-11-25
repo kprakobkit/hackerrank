094b8c1b70c9f5a2c7a69e2fc1c2f3c33d158970
Peter Prakobkit <kprakobkit@gmail.com>
2014-11-24 23:01:17 -0800

    refactor once again

--- solutions/ruby/find-digits.rb
+++ solutions/ruby/find-digits.rb
@@ -1,8 +1,13 @@
 def find_digits(number)
   digits = number.chars.map(&:to_i)
+  total = 0
 
-  result = digits.reject{ |digit| digit == 0 }.inject(0) { |result, digit| result += 1 if number.to_i % digit == 0 }
+  digits.reject{ |digit| digit == 0 }.each{ |digit| total += 1 if number.to_i % digit == 0 }
+
+  total
 end
 
 p find_digits("12")
 p find_digits("1012")
+p find_digits("123456789")
+p find_digits("114108089")
