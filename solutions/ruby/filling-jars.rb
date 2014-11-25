cd624f9acea4c38232656d14a7ab4c353cec42e7
Peter Prakobkit <kprakobkit@gmail.com>
2014-11-24 19:37:18 -0800

    rename variables so they don't shadow each other

--- solutions/ruby/filling-jars.rb
+++ solutions/ruby/filling-jars.rb
@@ -1,10 +1,10 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 
 no_of_jars, no_of_operations = gets.split(' ').map(&:to_i)
-total =  no_of_operations.times.inject(0) do |total|
+total_candies =  no_of_operations.times.inject(0) do |total|
   starting_index, ending_index, no_of_candies = gets.split(' ').map(&:to_i)
   total += (ending_index - starting_index + 1) * no_of_candies
 end
 
-puts total/no_of_jars
+puts total_candies/no_of_jars
 
